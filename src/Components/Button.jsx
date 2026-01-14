c2b718c63a487ec9f97259a1ce206309bcf0cad5
Damien <lyrien_anx@yahoo.com>
2026-01-14 23:30:30 +0800

    button commit

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -4,7 +4,7 @@ function Button(){
     return(
         <div className='d-grid gap-2 col-6 mx-auto my-2'>
             <button className='btn' style={{backgroundColor:'#ABA361'}}>
-                Click Here
+                Click Here Now
             </button>
         </div>
     )
